2f745284e4a3f88b9ef36524e9d24b9972771b3a
ibahadiraltun <ibahadiraltun@gmail.com>
2021-08-17 15:56:37 +0200

    update&delete&add for sell_products

--- app.py
+++ app.py
@@ -25,12 +25,12 @@ Product = db.Table('products', db.metadata, autoload=True, autoload_with=db.engi
 @app.route('/', methods=['GET', 'POST'])
 def login():
   if request.method == 'POST':
-    email= request.form['email']
-    password= request.form['password']
-    query= select([User.c.password]).where(User.c.email == email)
-    db_password= conn.execute(query).fetchone()
+    email = request.form['email']
+    password = request.form['password']
+    query = select([User.c.password]).where(User.c.email == email)
+    db_password = conn.execute(query).fetchone()
 
-    query= select([User.c.userid]).where(User.c.email == email)
+    query = select([User.c.userid]).where(User.c.email == email)
     global user_id
     user_id = conn.execute(query).fetchone()[0]
     
@@ -39,8 +39,8 @@ def login():
     else:
       error = 'Wrong password or email'
       return render_template('login.html', error=error)
-  else:   
-    return render_template('login.html')
+
+  return render_template('login.html')
 
 @app.route('/main', methods=['GET'])
 def main():
@@ -49,36 +49,36 @@ def main():
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
   if request.method == 'POST':
-    name=request.form['name']
-    lastname=request.form['lastname']
-    email=request.form['email']
-    phone=request.form['phone']
-    address=request.form['address']
-    password=request.form['password']
-    confirmpassword=request.form['confirmpassword']
-    error=''
-    if(password != confirmpassword):
+    name = request.form['name']
+    lastname = request.form['lastname']
+    email = request.form['email']
+    phone = request.form['phone']
+    address = request.form['address']
+    password = request.form['password']
+    confirmpassword = request.form['confirmpassword']
+    error = ''
+    if (password != confirmpassword):
       error = 'Passwords do not match!!' 
 
     if(error == ''):
-      new_user= User.insert().values(
-        name=name,
-        lname=lastname,
-        email=email,
-        password=password,
-        phone=phone,
-        address=address
+      new_user = User.insert().values(
+        name = name,
+        lname = lastname,
+        email = email,
+        password = password,
+        phone = phone,
+        address = address
       )
       conn.execute(new_user)
       return redirect(url_for('login'))
     else:
       return render_template('signup.html')
-  else:
-    return render_template('signup.html')
+
+  return render_template('signup.html')
 
 @app.route('/sell_product' , methods=['GET'])
 def sell_product():
-  query=select([Product]).where(Product.c.owner_id == user_id)
+  query = select([Product]).where(Product.c.owner_id == user_id)
   sellable_products = conn.execute(query).fetchall()
   return render_template('sellProduct.html', products = sellable_products)
 
@@ -94,13 +94,65 @@ def update_budget():
 def update_user():
   return render_template('updateUser.html')
 
-@app.route('/update_product' , methods=['GET'])
-def update_product():
-  return render_template('updateProduct.html')
+@app.route('/add_product', methods=['GET', 'POST'])
+def add_product():
+  if request.method == 'POST':
+    name = request.form['name']
+    sname = request.form['shortname']
+    category = request.form['category']
+    publish_date = request.form['publish_date']
+    price = request.form['price']
+    stock = request.form['stock']
 
-@app.route('/delete_product' , methods=['GET'])
-def delete_product():
-  return render_template('deleteProduct.html')
+    owner_id = user_id
+    query = Product.insert().values(
+      owner_id = owner_id,
+      name = name,
+      shortname = sname,
+      category = category,
+      publish_date = publish_date,
+      price = price,
+      stock = stock
+    )
+    conn.execute(query)
+    return redirect(url_for('sell_product'))
+  
+  return render_template('updateProduct.html', product = {})
+
+@app.route('/update_product/<product_id>' , methods=['GET', 'POST'])
+def update_product(product_id):
+  query = select([Product]).where(Product.c.product_id == product_id)
+  product = conn.execute(query).fetchone()
+
+  if request.method == 'POST':
+    name = request.form['name']
+    sname = request.form['shortname']
+    category = request.form['category']
+    publish_date = request.form['publish_date']
+    price = request.form['price']
+    stock = request.form['stock']
+
+    owner_id = user_id
+    query = update(Product).where(Product.c.product_id == product_id).values(
+      product_id = product_id,
+      owner_id = owner_id,
+      name = name,
+      shortname = sname,
+      category = category,
+      publish_date = publish_date,
+      price = price,
+      stock = stock
+    )
+    conn.execute(query)
+    return redirect(url_for('sell_product'))
+  
+  return render_template('updateProduct.html', product = product)
+
+@app.route('/delete_product/<product_id>' , methods=['GET'])
+def delete_product(product_id):
+  query = Product.delete().where(Product.c.product_id == product_id)
+  conn.execute(query)
+  return redirect(url_for('sell_product'))
 
 if __name__ == '__main__':
-   app.run(debug = True)
+  app.run(debug = True)
